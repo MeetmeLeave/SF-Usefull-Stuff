eaeb82f886fa619690e572c531fed964987b78e5
Vlad M <vladimir.martynenko.work@gmail.com>
2016-05-14 20:46:04 +0300

    Code in this repo is smelly, need to refactor it some day as of right now it is one time utilities, writtern to automate particular task, instead of point and clicking for ages.

---
+++ SFEnhancer/ConsoleApplication1/DependentPickListGenerator.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    public static class DependentPickListGenerator
+    {
+        public static void PickDependentPicklists(string path, string outPath)
+        {
+            string text = System.IO.File.ReadAllText(path);
+            string[] stringSeparators = new string[] { "\r\n" };
+            string[] core = text.Split(stringSeparators, StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<string, string> pickListDependencies = new Dictionary<string, string>();
+
+            foreach (var str in core)
+            {
+                stringSeparators[0] = ("\t");
+                string[] picklistCombo = str.Split(stringSeparators, StringSplitOptions.RemoveEmptyEntries);
+
+                string key = picklistCombo[1].Replace("\"", "");
+                string controling = picklistCombo[0].Replace("\"", "");
+
+                if (!pickListDependencies.ContainsKey(key))
+                {
+                    string temp = "<picklistValues>";
+                    temp += "<fullName>" + key + "</fullName>";
+                    temp += "<controllingFieldValues>" + controling + "</controllingFieldValues>";
+                    pickListDependencies.Add(key, temp);
+                }
+                else {
+                    pickListDependencies[key] += "<controllingFieldValues>" + controling + "</controllingFieldValues>";
+                }
+            }
+
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine("<fields>");
+            builder.AppendLine("<fullName>Infustry_Type__c</fullName>");
+            builder.AppendLine("<externalId>false</externalId>");
+            builder.AppendLine("<label>Infustry Type</label>");
+            builder.AppendLine("<picklist>");
+            builder.AppendLine("<controllingField>Industry</controllingField>");
+
+            foreach (var key in pickListDependencies.Keys)
+            {
+                builder.AppendLine(pickListDependencies[key]);
+                builder.AppendLine("<default>false</default>");
+                builder.AppendLine("</picklistValues>");
+            }
+
+            builder.AppendLine("<sorted>false</sorted>");
+            builder.AppendLine("</picklist>");
+            builder.AppendLine("<trackFeedHistory>false</trackFeedHistory>");
+            builder.AppendLine("<type>Picklist</type>");
+            builder.AppendLine("</fields>");
+
+            System.IO.File.WriteAllText(outPath, builder.ToString());
+        }
+    }
+}
--- SFEnhancer/ConsoleApplication1/Program.cs
+++ SFEnhancer/ConsoleApplication1/Program.cs
@@ -9,9 +9,10 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            DependentPickListGenerator.PickDependentPicklists(@"D:\Domain model detalization (commented).txt", @"D:\result1.txt");
             //CodeGen.GenerateInsertStatementForSWAPI(@"D:\fields3.txt");
             //CodeGen.ReverseCode(@"D:\fields2.txt");
-            FieldsGen.GenerateFields(@"D:\test1.txt", @"D:\result1.txt");
+            //FieldsGen.GenerateFields(@"D:\test1.txt", @"D:\result1.txt");
             //FieldsGen.GenerateFields(@"D:\OCPMonthlyExtract_DecemberSample.csv", @"D:\result.txt");
             //CodeGen.GenerateCode(@"D:\source.txt", "ocpCase", "fileRecord");
             //CodeGen.GenerateAnonymousCode(@"D:\OCPMonthlyExtract_DecemberSample.csv", "ocp");
